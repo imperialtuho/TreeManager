9c227cd042ff57308de0c2fc0413ee53150cac08
Imperial Tu Ho <waffenss436@gmail.com>
2021-08-07 20:40:41 +0700

    Small patch update

--- src/public/js/Chart.js
+++ src/public/js/Chart.js
@@ -100,8 +100,6 @@ MapDataPushFetch()
 async function drawChart() {
     await MapDataPushFetch()
     await setLabels()
-    myLineChart1.data.datasets[0].data = windData1
-    myLineChart1.data.datasets[1].data = windData2
     myLineChart1.update()
     console.log('Updated')
 }
@@ -109,7 +107,8 @@ async function drawChart() {
 /* Set Hour labels */
 
 var hours = ["00:00 AM", "01:00 AM", "02:00 AM", "03:00 AM", "04:00 AM", "05:00 AM",
-    "06:00 AM", "07:00 AM", "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 AM"
+    "06:00 AM", "07:00 AM", "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM", "02:00 PM", "03:00 PM", "04:00 PM", "05:00 PM",
+    "06:00 PM", "07:00 PM", "08:00 PM", "09:00 PM", "10:00 PM", "11:00 PM"
 ];
 async function setLabels() {
     var nextMonthIndex = hours.indexOf(myLineChart1.data.labels[myLineChart1.data.labels.length - 1]) + 1;
